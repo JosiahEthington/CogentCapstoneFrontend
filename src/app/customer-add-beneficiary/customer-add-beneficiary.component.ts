16ba2a9828057bcd6c816a22088fddf5c6a71e4b
JosiahEthington <31279433+JosiahEthington@users.noreply.github.com>
2022-03-14 14:17:42 -0500

    Removed merge artifacts

--- src/app/customer-add-beneficiary/customer-add-beneficiary.component.ts
+++ src/app/customer-add-beneficiary/customer-add-beneficiary.component.ts
@@ -1,13 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< Updated upstream
 import { AddBeneficiaryRequest } from '../models/request/AddBeneficiaryRequest';
-=======
->>>>>>> Stashed changes
 
 @Component({
   selector: 'app-customer-add-beneficiary',
   templateUrl: './customer-add-beneficiary.component.html',
-<<<<<<< Updated upstream
   styleUrls: ['./customer-add-beneficiary.component.css'],
 })
 export class CustomerAddBeneficiaryComponent implements OnInit {
@@ -15,15 +11,4 @@ export class CustomerAddBeneficiaryComponent implements OnInit {
 
   ngOnInit(): void {}
   addBeneficiaryRequest: AddBeneficiaryRequest = new AddBeneficiaryRequest();
-=======
-  styleUrls: ['./customer-add-beneficiary.component.css']
-})
-export class CustomerAddBeneficiaryComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
->>>>>>> Stashed changes
 }
