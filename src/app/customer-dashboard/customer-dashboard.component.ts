547d0561d71c756b93769a68003b1bbc94fd6c89
rakesh <rakeshreddy123@gmail.com>
2022-03-13 21:56:07 -0500

    dashboard component

--- src/app/customer-dashboard/customer-dashboard.component.ts
+++ src/app/customer-dashboard/customer-dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+<<<<<<< Updated upstream
+=======
+import { AccountSummaryResponse } from '../models/response/AccountSummaryResponse';
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-customer-dashboard',
@@ -8,8 +12,34 @@ import { Component, OnInit } from '@angular/core';
 export class CustomerDashboardComponent implements OnInit {
 
   constructor() { }
+<<<<<<< Updated upstream
 
   ngOnInit(): void {
   }
 
+=======
+  myClick(){
+    console.log('Welcome Rama')
+  }
+  EventFunction(){
+    alert("Transfer the Amount");
+  }
+
+  ngOnInit(): void {
+    this.listOfAccounts[0].accountType='SB'
+    this.listOfAccounts[0].balance=3234
+    this.listOfAccounts[0].status='enabled'
+
+    this.listOfAccounts[1].accountType='CA'
+    this.listOfAccounts[1].balance=12235
+    this.listOfAccounts[1].status='enabled'
+   
+   
+  }
+
+  displayAccountDetails():void{
+    
+  }
+  listOfAccounts:AccountSummaryResponse[]=[new AccountSummaryResponse(),new AccountSummaryResponse()]
+>>>>>>> Stashed changes
 }
