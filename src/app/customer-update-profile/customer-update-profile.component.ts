0f2d4dbc575de7b1f7e6c0d5eeed27ec4f7084e3
JosiahEthington <31279433+JosiahEthington@users.noreply.github.com>
2022-03-14 14:20:56 -0500

    Removed merge artifacts

--- src/app/customer-update-profile/customer-update-profile.component.ts
+++ src/app/customer-update-profile/customer-update-profile.component.ts
@@ -1,24 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< Updated upstream
-=======
 import { UpdateCustomerRequest } from '../models/request/UpdateCustomerRequest';
 import { UpdateCustomerResponse } from '../models/response/UpdateCustomerResponse';
->>>>>>> Stashed changes
 
 @Component({
   selector: 'app-customer-update-profile',
   templateUrl: './customer-update-profile.component.html',
-<<<<<<< Updated upstream
-  styleUrls: ['./customer-update-profile.component.css']
-})
-export class CustomerUpdateProfileComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-=======
   styleUrls: ['./customer-update-profile.component.css'],
 })
 export class CustomerUpdateProfileComponent implements OnInit {
@@ -31,5 +17,4 @@ export class CustomerUpdateProfileComponent implements OnInit {
   updateCustomerResponse: UpdateCustomerResponse = new UpdateCustomerResponse();
 
   updateCustomer(): void {}
->>>>>>> Stashed changes
 }
