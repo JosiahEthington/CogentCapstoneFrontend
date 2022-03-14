79f49c3fae86b2708fe7315c94f412be00c3d61e
JosiahEthington <31279433+JosiahEthington@users.noreply.github.com>
2022-03-14 14:24:22 -0500

    Removed merge artifacts

--- src/app/models/response/AccountDetailsResponse.ts
+++ src/app/models/response/AccountDetailsResponse.ts
@@ -1,16 +1,9 @@
-<<<<<<< Updated upstream
-=======
 import { Transaction } from '../Transaction';
 
->>>>>>> Stashed changes
 export class AccountDetailsResponse {
   accountNumber: number = 0;
   accountType: string = '';
   status: string = '';
-<<<<<<< Updated upstream
-  transactions: any = {};
-=======
   balance: number = 0;
   transactions: Transaction[] = [];
->>>>>>> Stashed changes
 }
