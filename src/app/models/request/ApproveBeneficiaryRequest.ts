f0332562564e7423e5ae7a314255e9d4260d57c3
JosiahEthington <31279433+JosiahEthington@users.noreply.github.com>
2022-03-14 14:23:43 -0500

    Removed merge artifacts

--- src/app/models/request/ApproveBeneficiaryRequest.ts
+++ src/app/models/request/ApproveBeneficiaryRequest.ts
@@ -1,8 +1,4 @@
-<<<<<<< Updated upstream
 export class ApproveBeneficiaryRequest {
-=======
-export class AddBeneficiaryRequest {
->>>>>>> Stashed changes
   fromCustomer: number = 0;
   toCustomer: number = 0;
   beneficiaryAddedDate: Date = new Date();
