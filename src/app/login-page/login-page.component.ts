70f33eb370b5e92005bae8ec1230b42fac0d82e4
JosiahEthington <31279433+JosiahEthington@users.noreply.github.com>
2022-03-14 14:22:59 -0500

    Removed merge artifacts

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginRequest } from '../models/request/LoginRequest';
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -13,10 +10,7 @@ export class LoginPageComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
   loginRequest: LoginRequest = new LoginRequest();
   customerLogin(): void {}
 }
